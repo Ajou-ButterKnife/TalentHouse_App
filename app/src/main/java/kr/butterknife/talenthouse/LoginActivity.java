9ad6bfedbc2c7542e05c7010b9cb578ee9b80e18
GodSang <tkdgh584@gmail.com>
2021-04-06 00:42:18 +0900

    login page version1

--- app/src/main/java/kr/butterknife/talenthouse/LoginActivity.java
+++ app/src/main/java/kr/butterknife/talenthouse/LoginActivity.java
@@ -3,6 +3,8 @@ package kr.butterknife.talenthouse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginButtonClick(View view) {
+        Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
     }
 }
