250da703eaa690aebe0d43baf8d883e5668015a8
KHS-kr <ks96ks@ajou.ac.kr>
2021-04-06 18:46:43 +0900

    add retrofit in SignUpActivity, not work(only transfer data)

--- app/src/main/java/kr/butterknife/talenthouse/SignUpActivity.java
+++ app/src/main/java/kr/butterknife/talenthouse/SignUpActivity.java
@@ -3,11 +3,22 @@ package kr.butterknife.talenthouse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import kr.butterknife.talenthouse.network.ButterKnifeApi;
+import kr.butterknife.talenthouse.network.request.LoginReq;
+import kr.butterknife.talenthouse.network.request.SignUpReq;
+import kr.butterknife.talenthouse.network.response.LoginRes;
+import kr.butterknife.talenthouse.network.response.SignUpRes;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private String TAG = "SIGN_UP_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +27,39 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void btn_click(View view) {
-        EditText editTextEmail = findViewById(R.id.signup_et_email);
-        EditText editTextPassword = findViewById(R.id.signup_et_password);
-        EditText editTextPhone = findViewById(R.id.signup_et_phonenumber);
-        Toast.makeText(getApplicationContext(), editTextEmail.getText().toString(), Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), editTextPassword.getText().toString(), Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), editTextPhone.getText().toString(), Toast.LENGTH_LONG).show();
+        String email = ((EditText) findViewById(R.id.signup_et_email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.signup_et_password)).getText().toString();
+        String phone = ((EditText) findViewById(R.id.signup_et_phonenumber)).getText().toString();
+
+        new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ButterKnifeApi.INSTANCE.getRetrofitService().addUser(new SignUpReq(email, password, phone)).enqueue(new Callback<SignUpRes>() {
+                        @Override
+                        public void onResponse(Call<SignUpRes> call, Response<SignUpRes> response) {
+                            // 정상 출력이 되면 아래 로그가 출력됨
+                            if(response.body() != null)
+                                Log.d(TAG, response.body().getEmail());
+                                // 정상 출력이 되지 않을 때 서버에서의 response
+                            else {
+                                Log.d(TAG, response.errorBody().toString());
+                                Log.d(TAG, response.message());
+                                Log.d(TAG, String.valueOf(response.code()));
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<SignUpRes> call, Throwable t) {
+                            // 서버쪽으로 아예 메시지를 보내지 못한 경우
+                            Log.d(TAG, "SERVER CONNECTION ERROR");
+                        }
+                    });
+                }
+                catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.run();
     }
 }
