e754dcf83f47dc56ca440a31ce81657c5135bb4f
GodSang <tkdgh584@gmail.com>
2021-04-06 13:39:12 +0900

    add Sign Up button

--- app/src/main/java/kr/butterknife/talenthouse/LoginActivity.java
+++ app/src/main/java/kr/butterknife/talenthouse/LoginActivity.java
@@ -2,6 +2,7 @@ package kr.butterknife.talenthouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,5 +17,10 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onLoginButtonClick(View view) {
         Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSignUpButtonClick(View view) {
+        Intent i1 = new Intent (getApplicationContext(), SignUpActivity.class);
+        startActivity(i1);
     }
 }
