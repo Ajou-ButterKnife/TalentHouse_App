7584297c785ad09ea1841bfe7cd412f454e3be7a
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2021-04-17 21:57:24 +0900

    Update CommonResponse.kt
    modify error

--- app/src/main/java/kr/butterknife/talenthouse/network/response/CommonResponse.kt
+++ app/src/main/java/kr/butterknife/talenthouse/network/response/CommonResponse.kt
@@ -1,11 +1,6 @@
 package kr.butterknife.talenthouse.network.response
 
 data class CommonResponse(
-<<<<<<< HEAD
-        val result : String,
-        val detail : String?
-=======
     val result : String,
     val detail : String?
->>>>>>> 25418641447b28341cd79461104cdcbe5663c9a7
 )
