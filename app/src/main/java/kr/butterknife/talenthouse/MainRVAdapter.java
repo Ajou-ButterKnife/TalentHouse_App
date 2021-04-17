0f18d6810cebca377eaf9e2deb16404b55d103e1
KHS-kr <ks96ks@ajou.ac.kr>
2021-04-17 23:13:26 +0900

    divide viewholder anothe class, divide all viewholder use view type for mp3, mp4, image

--- app/src/main/java/kr/butterknife/talenthouse/MainRVAdapter.java
+++ app/src/main/java/kr/butterknife/talenthouse/MainRVAdapter.java
@@ -6,53 +6,85 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import kr.butterknife.talenthouse.MainRVViewHolder.*;
 
 import java.util.ArrayList;
 
-public class MainRVAdapter extends RecyclerView.Adapter<MainRVAdapter.MyViewHolder> {
+public class MainRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private ArrayList<RVItem> arrayList;
-
-    static class MyViewHolder extends RecyclerView.ViewHolder {
-        protected TextView title;
-        protected TextView writer;
-        protected TextView date;
-        protected TextView subject;
-
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-            title = itemView.findViewById(R.id.rvtext_tv_title);
-            writer = itemView.findViewById(R.id.rvtext_tv_writer);
-            date = itemView.findViewById(R.id.rvtext_tv_date);
-            subject = itemView.findViewById(R.id.rvtext_tv_subject);
-        }
-    }
 
     public MainRVAdapter(ArrayList<RVItem> list) {
         arrayList = list;
     }
 
-    // TODO: 4/6/21 나중에 들어오는 타입에 따라서 뷰 여러개
-    // 참고 : https://lakue.tistory.com/16
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rv_text, parent, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = null;
 
-        MyViewHolder vh = new MyViewHolder(view);
+        if(viewType == ContentType.MP3.ordinal()) {
+            LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rv_mp3, parent, false);
+            ContentMP3ViewHolder vh = new ContentMP3ViewHolder(view);
+            return vh;
+        }
+        else if(viewType == ContentType.MP4.ordinal()) {
+            LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rv_mp4, parent, false);
+            ContentMP4ViewHolder vh = new ContentMP4ViewHolder(view);
+            return vh;
+        }
+        else if(viewType == ContentType.IMAGE.ordinal()) {
+            LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rv_image, parent, false);
+            ContentImageViewHolder vh = new ContentImageViewHolder(view);
+            return vh;
+        }
 
+        LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rv_text, parent, false);
+        ContentNOViewHolder vh = new ContentNOViewHolder(view);
         return vh;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.title.setText(arrayList.get(position).getTitle());
-        holder.writer.setText(arrayList.get(position).getWriter());
-        holder.date.setText(arrayList.get(position).getDate());
-        holder.subject.setText(arrayList.get(position).getSubject());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder _holder, int position) {
+        if(position == ContentType.NO.ordinal()) {
+            ContentNOViewHolder holder = (ContentNOViewHolder) _holder;
+            holder.title.setText(arrayList.get(position).getTitle());
+            holder.writer.setText(arrayList.get(position).getWriter());
+            holder.date.setText(arrayList.get(position).getDate());
+            holder.subject.setText(arrayList.get(position).getSubject());
+        }
+        else if(position == ContentType.MP3.ordinal()) {
+
+        }
+        else if(position == ContentType.MP4.ordinal()) {
+
+        }
+        else if(position == ContentType.IMAGE.ordinal()) {
+
+        }
+        else {
+
+        }
     }
 
     @Override
     public int getItemCount() {
         return arrayList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        RVItem item = arrayList.get(position);
+
+        if(item.getMp3Url() != null) {
+            return ContentType.MP3.ordinal();
+        }
+        else if(item.getMp4Url() != null) {
+            return ContentType.MP4.ordinal();
+        }
+        else if(item.getImageUrl() != null) {
+            return ContentType.IMAGE.ordinal();
+        }
+
+        return ContentType.NO.ordinal();
     }
 }
