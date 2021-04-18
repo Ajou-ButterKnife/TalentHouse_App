efcf138b1d394452966b2a323d19406d02d4203a
KHS-kr <ks96ks@ajou.ac.kr>
2021-04-18 14:02:06 +0900

    add write content fragment

---
+++ app/src/main/java/kr/butterknife/talenthouse/WriteFragment.java
@@ -0,0 +1,23 @@
+package kr.butterknife.talenthouse;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WriteFragment extends Fragment {
+
+    public WriteFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_write, container, false);
+    }
+}
