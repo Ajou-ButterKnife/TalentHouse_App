10fec58aefb233a78262e8228a788d3768b6f089
KHS-kr <ks96ks@ajou.ac.kr>
2021-04-25 15:02:47 +0900

    change data class name, add parameter, add annotation for json

--- app/src/main/java/kr/butterknife/talenthouse/DataTypes.kt
+++ app/src/main/java/kr/butterknife/talenthouse/DataTypes.kt
@@ -1,25 +1,47 @@
 package kr.butterknife.talenthouse
 
-data class RVItem(
+import com.squareup.moshi.Json
+
+data class PostItem(
     val title : String,
-    val writer : String,
-    val date : String,
-    val subject : String,
-    val mp3Url : String? = null,
-    val mp4Url : String? = null,
-    val imageUrl : String? = null
+    @Json(name = "writer_nickname")
+    val writerNickname : String,
+    @Json(name = "writer_id")
+    val writerId : String,
+    @Json(name = "update_time")
+    val updateTime : String,
+    val description : String,
+    var mp3Url : String? = null,
+    var videoUrl : String? = null,
+    val imageUrl : List<String>? = null
 ) {
-    constructor(title: String, writer: String, date: String, subject: String) : this(
-        title, writer, date, subject, null, null, null
+    constructor(title : String, writerNickname : String, writerId : String, updateTime : String, description : String) : this (
+        title, writerNickname, writerId, updateTime, description, null, null, null
+    )
+
+    constructor(title : String, writerNickname : String, writerId : String, updateTime : String, description : String, url : String) : this (
+        title, writerNickname, writerId, updateTime, description, null, null, null
+    ) {
+        if(url.contains(".mp4"))
+            this.videoUrl = url
+        else
+            this.mp3Url = url
+    }
+
+    constructor(title : String, writerNickname : String, writerId : String, updateTime : String, description : String, imageUrl: List<String>) : this (
+        title, writerNickname, writerId, updateTime, description, null, null, imageUrl
     )
 }
 
 enum class ContentType(type : Int) {
-    NO(0), IMAGE(1), MP3(2), MP4(3)
+    NO(0), IMAGE(1), MP3(2), VIDEO(3)
 }
 
 data class CommentItem(
-    val writer : String,
+    @Json(name = "writer_id")
+    val writerId : String,
+    @Json(name = "writer_nickname")
+    val writerNickname : String,
     val date : String,
     val comment : String,
 )
