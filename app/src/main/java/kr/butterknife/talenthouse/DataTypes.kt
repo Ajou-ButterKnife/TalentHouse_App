b493b9a0b1d44d197324e57cfc91c40b653d8ae7
KHS-kr <ks96ks@ajou.ac.kr>
2021-04-25 16:08:12 +0900

    add data type

--- app/src/main/java/kr/butterknife/talenthouse/DataTypes.kt
+++ app/src/main/java/kr/butterknife/talenthouse/DataTypes.kt
@@ -1,6 +1,7 @@
 package kr.butterknife.talenthouse
 
 import com.squareup.moshi.Json
+import java.util.*
 
 data class PostItem(
     val title : String,
@@ -9,18 +10,22 @@ data class PostItem(
     @Json(name = "writer_id")
     val writerId : String,
     @Json(name = "update_time")
-    val updateTime : String,
+    val updateTime : Date,
     val description : String,
     var mp3Url : String? = null,
     var videoUrl : String? = null,
-    val imageUrl : List<String>? = null
+    val imageUrl : List<String>? = null,
+    @Json(name = "likes_cnt")
+    val likesCnt : Int,
+    val category : String,
+    val comments : List<CommentItem>
 ) {
-    constructor(title : String, writerNickname : String, writerId : String, updateTime : String, description : String) : this (
-        title, writerNickname, writerId, updateTime, description, null, null, null
+    constructor(title : String, writerNickname : String, writerId : String, updateTime : Date, description : String, likesCnt : Int, category : String, comments : List<CommentItem>) : this (
+        title, writerNickname, writerId, updateTime, description, null, null, null, likesCnt, category, comments
     )
 
-    constructor(title : String, writerNickname : String, writerId : String, updateTime : String, description : String, url : String) : this (
-        title, writerNickname, writerId, updateTime, description, null, null, null
+    constructor(title : String, writerNickname : String, writerId : String, updateTime : Date, description : String, url : String, likesCnt : Int, category : String, comments : List<CommentItem>) : this (
+        title, writerNickname, writerId, updateTime, description, null, null, null, likesCnt, category, comments
     ) {
         if(url.contains(".mp4"))
             this.videoUrl = url
@@ -28,8 +33,8 @@ data class PostItem(
             this.mp3Url = url
     }
 
-    constructor(title : String, writerNickname : String, writerId : String, updateTime : String, description : String, imageUrl: List<String>) : this (
-        title, writerNickname, writerId, updateTime, description, null, null, imageUrl
+    constructor(title : String, writerNickname : String, writerId : String, updateTime : Date, description : String, imageUrl: List<String>, likesCnt : Int, category : String, comments : List<CommentItem>) : this (
+        title, writerNickname, writerId, updateTime, description, null, null, imageUrl, likesCnt, category, comments
     )
 }
 
