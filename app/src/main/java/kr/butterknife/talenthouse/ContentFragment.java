33178d1152c192b73d50943c4867a0fe00c563c3
GodSang <tkdgh584@gmail.com>
2021-05-01 18:06:37 +0900

    ViewPager를 사용하여 사진 게시글 조회 구현

--- app/src/main/java/kr/butterknife/talenthouse/ContentFragment.java
+++ app/src/main/java/kr/butterknife/talenthouse/ContentFragment.java
@@ -1,10 +1,12 @@
 package kr.butterknife.talenthouse;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +15,10 @@ import android.view.ViewStub;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class ContentFragment extends Fragment {
 
@@ -27,9 +29,17 @@ public class ContentFragment extends Fragment {
     RecyclerView commentRV;
     ArrayList<CommentItem> commentList;
     CommentRVAdapter rvAdapter;
+    Context context;
+    ImageContentPagerAdapter adapter;
+    ViewPager viewPager;
+    CircleIndicator indicator;
 
     public ContentFragment(PostItem item) {
         this.item = item;
+        List<String> imageUrl = new ArrayList<>();
+        imageUrl.add("https://talent-house-app.s3.ap-northeast-2.amazonaws.com/photo/608ce12de5955b344cc8f85c20210204_154101.jpg");
+        imageUrl.add("https://talent-house-app.s3.ap-northeast-2.amazonaws.com/photo/608ce18a15d3bcb383e3678eIMG_20210425_170553.jpg");
+        this.item.setImageUrl(imageUrl);
     }
 
     @Override
@@ -42,11 +52,12 @@ public class ContentFragment extends Fragment {
         comment = view.findViewById(R.id.content_et_comment);
         commentRV = view.findViewById(R.id.content_rv_comment);
         addComment = view.findViewById(R.id.content_btn_addcomment);
+
         setIncludeLayout();
 
         commentList = new ArrayList<>();
         for(int i = 0; i < 20; i++)
-            commentList.add(new CommentItem(LoginInfo.INSTANCE.getLoginInfo(getContext()), "writer" + (i + 1), "1234", "This is comment. Comment's number is " + (i + 1)));
+            commentList.add(new CommentItem(LoginInfo.INSTANCE.getLoginInfo(getContext())[0], "writer" + (i + 1), "1234", "This is comment. Comment's number is " + (i + 1)));
 
         rvAdapter = new CommentRVAdapter(commentList);
 
@@ -65,7 +76,18 @@ public class ContentFragment extends Fragment {
 
         }
         else if(item.getImageUrl() != null) {
-
+            content.setLayoutResource(R.layout.viewstub_content_image);
+            View inflated = content.inflate();
+            context = getContext();
+            title = inflated.findViewById(R.id.content_image_title);
+            date = inflated.findViewById(R.id.content_image_date);
+            writer = inflated.findViewById(R.id.content_image_tv_writer);
+            subject = inflated.findViewById(R.id.content_tv_subject);
+            viewPager = inflated.findViewById(R.id.content_pager);
+            adapter = new ImageContentPagerAdapter(context, item.getImageUrl());
+            viewPager.setAdapter(adapter);
+            indicator = inflated.findViewById(R.id.content_indicator);
+            indicator.setViewPager(viewPager);
         }
         else if(item.getVideoUrl() != null) {
 
@@ -87,7 +109,7 @@ public class ContentFragment extends Fragment {
     }
 
     public void writeComment() {
-        CommentItem newComment = new CommentItem(LoginInfo.INSTANCE.getLoginInfo(getContext()), "test", "1234", comment.getText().toString());
+        CommentItem newComment = new CommentItem(LoginInfo.INSTANCE.getLoginInfo(getContext())[0], "test", "1234", comment.getText().toString());
 
         //network 작업
 
