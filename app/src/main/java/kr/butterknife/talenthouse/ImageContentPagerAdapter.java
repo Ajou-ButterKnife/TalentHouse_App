57636a1959fbc5efcdb81ca466f7034750c9bcaf
GodSang <tkdgh584@gmail.com>
2021-05-01 18:23:10 +0900

    Glide 오류 제거 및 테스트 코드 제거

--- app/src/main/java/kr/butterknife/talenthouse/ImageContentPagerAdapter.java
+++ app/src/main/java/kr/butterknife/talenthouse/ImageContentPagerAdapter.java
@@ -14,7 +14,6 @@ import com.bumptech.glide.Glide;
 import java.util.List;
 
 public class ImageContentPagerAdapter extends PagerAdapter {
-    private int[] images = {R.drawable.capture001, R.drawable.capture002};
     private LayoutInflater inflater;
     private Context context;
     private List<String> imageUrl;
@@ -26,7 +25,7 @@ public class ImageContentPagerAdapter extends PagerAdapter {
     }
     @Override
     public int getCount() {
-        return images.length;
+        return imageUrl.size();
     }
 
     @Override
@@ -40,7 +39,6 @@ public class ImageContentPagerAdapter extends PagerAdapter {
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.item_content_image, container, false);
         ImageView imageView = view.findViewById(R.id.vp_iv_image);
-//        imageView.setImageResource(images[position]);
 
         Glide.with(context)
                 .load(imageUrl.get(position))
