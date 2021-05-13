6ebaaaca22e5a084d92e990c767164b060f70203
KHS-kr <ks96ks@ajou.ac.kr>
2021-05-14 00:32:59 +0900

    add api, sort

--- app/src/main/java/kr/butterknife/talenthouse/network/ApiService.kt
+++ app/src/main/java/kr/butterknife/talenthouse/network/ApiService.kt
@@ -12,8 +12,8 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
 
 //private const val BASE_URL = "http://3.137.162.68:4000/"
-//private const val BASE_URL = "http://10.0.2.2:4000/"
-private const val BASE_URL = "http://172.30.1.5:4000/"
+private const val BASE_URL = "http://10.0.2.2:4000/"
+//private const val BASE_URL = "http://172.30.1.5:4000/"
 
 private val loggingInterceptor = HttpLoggingInterceptor().apply {
     level = HttpLoggingInterceptor.Level.BODY
@@ -28,14 +28,14 @@ private val moshi = Moshi.Builder()
 
 interface ButterKnifeApiService {
 
-    @POST("signup/normal")
-    fun normalAddUser(@Body user : NormalSignUpReq) : Call<NormalSignUpRes>
-
     @POST("login/normal")
     fun login(@Body user : NormalLoginReq) : Call<NormalLoginRes>
 
     @POST("login/social")
     fun socialLogin(@Body uid : SocialLoginReq) : Call<SocialLoginRes>
+
+    @POST("signup/normal")
+    fun normalAddUser(@Body user : NormalSignUpReq) : Call<NormalSignUpRes>
 
     @POST("signup/nickname_overlap_check")
     fun overlapCheck(@Body data : OverlapNickname) : Call<CommonResponse>
@@ -48,9 +48,6 @@ interface ButterKnifeApiService {
 
     @GET("post")
     fun getPosts(@Query("category") category: String, @Query("page") page: Int) : Call<PostRes>
-  
-    @GET("user/category/{id}")
-    fun getCategories(@Path("id") key: String) : Call<CategoryRes>
 
     @POST("post/create")
     fun postCreate(@Body data : UploadPostReq) : Call<CommonResponse>
@@ -61,11 +58,23 @@ interface ButterKnifeApiService {
     @POST("post/comment/create")
     fun commentCreate(@Body data : UploadCommentReq) : Call<CommentRes>
 
-    @GET("user/{id}")
-    suspend fun getUserInfo(@Path("id") id : String) : MyPageRes
-
     @GET("post/{id}/{page}")
     suspend fun getMyPagePosts(@Path("id") id : String, @Path("page") page : Int) : PostRes
+
+    @GET("user/category/{id}")
+    fun getCategories(@Path("id") key: String) : Call<CategoryRes>
+
+    @GET("user/nickname/{id}")
+    suspend fun getUserNickname(@Path("id") id : String) : MyPageRes
+
+    @GET("user/{id}")
+    suspend fun getUserInfo(@Path("id") id : String) : UserInfoRes
+
+    @PUT("user/password/{id}")
+    suspend fun updatePassword(@Path("id") id : String, @Body data : PWUpdateReq) : CommonResponse
+
+    @PUT("user/{id}")
+    suspend fun updateInfo(@Path("id") id : String, @Body data : UserInfoUpdateReq) : CommonResponse
 }
 
 object ButterKnifeApi {
