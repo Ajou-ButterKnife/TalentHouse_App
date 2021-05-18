5744365b6cd9f98033914228aa643859a5c25f28
YongjinKim <dydwls0669@gmail.com>
2021-05-18 15:14:39 +0900

    add like btn

--- app/src/main/java/kr/butterknife/talenthouse/ContentFragment.java
+++ app/src/main/java/kr/butterknife/talenthouse/ContentFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +29,7 @@ import com.google.android.exoplayer2.ui.PlayerControlView;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -36,6 +38,11 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+
+import kr.butterknife.talenthouse.network.request.FavoriteUserIdReq;
+import kr.butterknife.talenthouse.network.request.GetCommentReq;
+import kr.butterknife.talenthouse.network.response.FavoritePostUserIdRes;
+import kr.butterknife.talenthouse.network.response.LikeRes;
 import me.relex.circleindicator.CircleIndicator;
 
 
@@ -54,7 +61,7 @@ public class ContentFragment extends Fragment {
 
     PostItem item;
     ViewStub content;
-    TextView title, date, writer, subject, addComment;
+    TextView title, date, writer, subject, addComment, like;
     EditText comment;
     RecyclerView commentRV;
     ArrayList<CommentItem> commentList;
@@ -65,6 +72,10 @@ public class ContentFragment extends Fragment {
     ImageContentPagerAdapter adapter;
     ViewPager viewPager;
     CircleIndicator indicator;
+    Button likeBtn;
+
+    BottomSheetDialog bottomSheetDialog;
+    FavoriteRVAdapter bottomAdapter;
 
     public ContentFragment(PostItem item) {
         this.item = item;
@@ -100,7 +111,7 @@ public class ContentFragment extends Fragment {
             @Override
             public void run(){
                 try{
-                    ButterKnifeApi.INSTANCE.getRetrofitService().getComments(item.get_id()).enqueue(new Callback<GetCommentsRes>() {
+                    ButterKnifeApi.INSTANCE.getRetrofitService().getComments(new GetCommentReq(item.get_id())).enqueue(new Callback<GetCommentsRes>() {
                         @Override
                         public void onResponse(Call<GetCommentsRes> call, Response<GetCommentsRes> response) {
                             if(response.body() != null){
@@ -130,10 +141,7 @@ public class ContentFragment extends Fragment {
 
 
     public void setIncludeLayout() {
-        if(item.getMp3Url() != null) {
-
-        }
-        else if(item.getImageUrl() != null) {
+        if(item.getImageUrl() != null) {
             content.setLayoutResource(R.layout.viewstub_content_image);
             View inflated = content.inflate();
             title = inflated.findViewById(R.id.content_image_title);
@@ -145,6 +153,8 @@ public class ContentFragment extends Fragment {
             viewPager.setAdapter(adapter);
             indicator = inflated.findViewById(R.id.content_indicator);
             indicator.setViewPager(viewPager);
+            like = inflated.findViewById(R.id.content_tv_like);
+            likeBtn = inflated.findViewById(R.id.content_btn_like);
         }
         else if(item.getVideoUrl() != null) {
             content.setLayoutResource(R.layout.viewstub_content_video);
@@ -155,6 +165,8 @@ public class ContentFragment extends Fragment {
             subject = inflated.findViewById(R.id.content_video_tv_subject);
             pv = inflated.findViewById(R.id.content_video_player);
             pcv = inflated.findViewById(R.id.content_video_controller);
+            like = inflated.findViewById(R.id.content_tv_like);
+            likeBtn = inflated.findViewById(R.id.content_btn_like);
         }
         else {
             content.setLayoutResource(R.layout.item_rv_text);
@@ -163,12 +175,119 @@ public class ContentFragment extends Fragment {
             date = inflated.findViewById(R.id.rvtext_tv_date);
             writer = inflated.findViewById(R.id.rvtext_tv_writer);
             subject = inflated.findViewById(R.id.rvtext_tv_subject);
+            like = inflated.findViewById(R.id.content_tv_like);
+            likeBtn = inflated.findViewById(R.id.content_btn_like);
         }
 
         title.setText(item.getTitle());
         date.setText(Util.INSTANCE.unixTime2String(Long.parseLong(item.getUpdateTime())));
         writer.setText(item.getWriterNickname());
         subject.setText(item.getDescription());
+
+        boolean check = false;
+        for(String id : item.getLikeIDs()){
+            if(id.equals(LoginInfo.INSTANCE.getLoginInfo(getContext())[0])){
+                check = true;
+                break;
+            }
+        }
+        if(check)
+            likeBtn.setText("좋아요 취소");
+        else
+            likeBtn.setText("좋아요");
+
+        like.setText("좋아요 " + item.getLikeCnt() + "개");
+        likeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Runnable(){
+                    @Override
+                    public void run() {
+                        try{
+                            ButterKnifeApi.INSTANCE.getRetrofitService().putLike(item.get_id(), LoginInfo.INSTANCE.getLoginInfo(getContext())[0]).enqueue(new Callback<LikeRes>() {
+                                @Override
+                                public void onResponse(Call<LikeRes> call, Response<LikeRes> response) {
+                                    if(response.body() != null){
+                                        if (response.body().getResult().equals("Plus")) {
+                                            like.setText("좋아요 " + response.body().getLikeCnt() + "개");
+                                            likeBtn.setText("좋아요 취소");
+                                        }else if(response.body().getResult().equals("Minus")){
+                                            like.setText("좋아요 " + response.body().getLikeCnt() + "개");
+                                            likeBtn.setText("좋아요");
+                                        }
+                                    }
+                                }
+                                @Override
+                                public void onFailure(Call<LikeRes> call, Throwable t) {
+                                    // 서버 쪽으로 메시지를 보내지 못한 경우
+                                    Log.d("err", "SERVER CONNECTION ERROR");
+                                }
+                            });
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }.run();
+            }
+        });
+
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetDialog = new BottomSheetDialog(getContext());
+                View v = getLayoutInflater().inflate(R.layout.bottom_content, null);
+                bottomSheetDialog.setContentView(v);
+
+                bottomSheetDialog.show();
+
+                RecyclerView recyclerView = v.findViewById(R.id.bottom_content_recyclerView);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                recyclerView.setLayoutManager(linearLayoutManager);
+
+                bottomAdapter = new FavoriteRVAdapter();
+                recyclerView.setAdapter(bottomAdapter);
+
+                getFavoriteUser(item.get_id());
+
+//                for(int i =0;i<5;i++){
+//                    likePerson l = new likePerson("nickname   :   " + i);
+//                    bottomAdapter.addItem(l);
+//                }
+//                bottomAdapter.notifyDataSetChanged();
+            }
+
+            public void getFavoriteUser(String postId){
+                new Runnable(){
+                    @Override
+                    public void run() {
+                        try{
+                            ButterKnifeApi.INSTANCE.getRetrofitService().getPostFavoriteId(new FavoriteUserIdReq(postId)).enqueue(new Callback<FavoritePostUserIdRes>() {
+                                @Override
+                                public void onResponse(Call<FavoritePostUserIdRes> call, Response<FavoritePostUserIdRes> response) {
+                                    if(response.body() != null){
+                                        List<String> userId =  response.body().getData();
+                                        for(String s : userId){
+                                            likePerson l = new likePerson(s);
+                                            bottomAdapter.addItem(l);
+                                        }
+                                        bottomAdapter.notifyDataSetChanged();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<FavoritePostUserIdRes> call, Throwable t) {
+                                    // 서버쪽으로 아예 메시지를 보내지 못한 경우
+                                    Log.d("ERR", "SERVER CONNECTION ERROR");
+                                }
+                            });
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }.run();
+            }
+
+        });
     }
 
     @Override
@@ -201,8 +320,6 @@ public class ContentFragment extends Fragment {
     }
 
     public void writeComment() {
-//        CommentItem newComment = new CommentItem(LoginInfo.INSTANCE.getLoginInfo(getContext())[0], "test", "1234", comment.getText().toString());
-
         //network 작업
         postComment(item.get_id(), LoginInfo.INSTANCE.getLoginInfo(getContext())[0], LoginInfo.INSTANCE.getLoginInfo(getContext())[1], comment.getText().toString());
         comment.setText("");
@@ -233,11 +350,9 @@ public class ContentFragment extends Fragment {
                                 Log.d("ERR", String.valueOf(response.code()));
                             }
                         }
-
                         @Override
                         public void onFailure(Call<CommentRes> call, Throwable t) {
                             // 서버쪽으로 아예 메시지를 보내지 못한 경우
-                            Toast.makeText(getActivity().getApplicationContext(), "서버와 통신이 원활하지 않습니다.", Toast.LENGTH_SHORT).show();
                             Log.d("ERR", "SERVER CONNECTION ERROR");
                         }
                     });
