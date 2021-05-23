8e0fd11355782a57bcb8129e38682fd85d011ad0
YongjinKim <dydwls0669@gmail.com>
2021-05-23 16:27:40 +0900

    add LoginInfo profile

--- app/src/main/java/kr/butterknife/talenthouse/LoginInfo.kt
+++ app/src/main/java/kr/butterknife/talenthouse/LoginInfo.kt
@@ -5,14 +5,16 @@ import android.content.Context
 private const val SPF_NAME = "ButterKnife"
 private const val LOGIN_ID = "LoginInfoId"
 private const val LOGIN_NAME = "LoginInfoName"
+private const val LOGIN_PROFILE = "LoginInfoProfile"
 private const val NOTIFICATION = "Noti-talenthouse"
 
 object LoginInfo {
-    fun setLoginInfo(id: String, nickname : String, context: Context) {
+    fun setLoginInfo(id: String, nickname : String, profile : String?, context: Context) {
         val spf = context.getSharedPreferences(SPF_NAME, Context.MODE_PRIVATE)
         val editor = spf.edit()
         editor.putString(LOGIN_ID, id)
         editor.putString(LOGIN_NAME, nickname)
+        editor.putString(LOGIN_PROFILE, profile)
         editor.apply()
     }
 
@@ -20,8 +22,9 @@ object LoginInfo {
         val spf = context.getSharedPreferences(SPF_NAME, Context.MODE_PRIVATE)
         val id = spf.getString(LOGIN_ID, "")!!
         val nickname = spf.getString(LOGIN_NAME, "")!!
+        val profile = spf.getString(LOGIN_PROFILE, "")!!
 
-        return arrayOf(id, nickname)
+        return arrayOf(id, nickname, profile)
     }
 
     fun logout(context : Context) {
@@ -30,6 +33,7 @@ object LoginInfo {
 
         editor.remove(LOGIN_ID)
         editor.remove(LOGIN_NAME)
+        editor.remove(LOGIN_PROFILE)
         editor.apply()
     }
 
